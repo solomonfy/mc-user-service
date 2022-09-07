44625f8b3d9adad7eb0fbcce8eb93abf1eb9aacc
solomonfy <fantahunsolomon@gmail.com>
2022-09-07 16:06:21 -0500

    added filter

--- MC_ORDER_MGT_BACKEND/api-gateway/src/main/java/com/medochemie/ordermanagement/apigateway/ApiGatewayApplication.java
+++ MC_ORDER_MGT_BACKEND/api-gateway/src/main/java/com/medochemie/ordermanagement/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.medochemie.ordermanagement.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,6 +15,16 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/").allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:4200");
+			}
+		};
 	}
 
 }
